f221c816d798dad7792e950c3980fd9721516ace
LilBib <vovchiksuper@yandex.ru>
2021-12-03 15:34:33 +0300

    Доработка проекта после отклонения от проверки

--- script/script.js
+++ script/script.js
@@ -4,12 +4,13 @@ let popupCloseButton = document.querySelector('.popup__close-icon');
 let popup = document.querySelector('.popup');
 let formName= document.querySelector('.form__item-name');
 let formDescription = document.querySelector('.form__item-description');
-let likeButtons = document.querySelectorAll('.element__like-button');
+let profileTitle=document.querySelector('.profile__title');
+let profileDescription=document.querySelector('.profile__description');
 
 
 function togglePopup() {
-    formName.value=document.querySelector('.profile__title').textContent;
-    formDescription.value=document.querySelector('.profile__description').textContent;
+    formName.value=profileTitle.textContent;
+    formDescription.value=profileDescription.textContent;
     if (popup.classList.contains('popup_opened')) {
     popup.classList.remove('popup_opened');
     }
@@ -23,19 +24,8 @@ function formSubmitHandler (evt) {
     document.querySelector('.profile__description').textContent=formDescription.value
     togglePopup();
 }
-function toggleLike(evt) {
-    if (evt.target.getAttribute('src')==="images/like.svg") {
-        evt.target.setAttribute('src',"images/like_active.svg");
-        
-    }
-    else {
-        evt.target.setAttribute('src',"images/like.svg");
-    }
-}
+
 
 formElement.addEventListener('submit', formSubmitHandler); 
 popupOpenButton.addEventListener('click', togglePopup);
 popupCloseButton.addEventListener('click', togglePopup);
-for (let i=0; i<likeButtons.length; ++i) {
-    likeButtons[i].addEventListener('click', toggleLike);
-}
