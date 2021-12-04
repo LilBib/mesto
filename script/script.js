a5e0934725e7a229415dca8c9e232eaad2ffb03a
LilBib <vovchiksuper@yandex.ru>
2021-12-04 15:43:48 +0300

    Исправление ошибок после первого ревью

--- script/script.js
+++ script/script.js
@@ -1,31 +1,30 @@
-let formElement=document.querySelector('.form');
+let formElement = document.querySelector('.form');
 let popupOpenButton = document.querySelector('.profile__edit-button');
 let popupCloseButton = document.querySelector('.popup__close-icon');
 let popup = document.querySelector('.popup');
-let formName= document.querySelector('.form__item-name');
-let formDescription = document.querySelector('.form__item-description');
-let profileTitle=document.querySelector('.profile__title');
-let profileDescription=document.querySelector('.profile__description');
+let formName = document.querySelector('.form__item_section_name');
+let formDescription = document.querySelector('.form__item_section_description');
+let profileTitle = document.querySelector('.profile__title');
+let profileDescription = document.querySelector('.profile__description');
 
 
-function togglePopup() {
-    formName.value=profileTitle.textContent;
-    formDescription.value=profileDescription.textContent;
-    if (popup.classList.contains('popup_opened')) {
+function openPopup() {
+    formName.value = profileTitle.textContent;
+    formDescription.value = profileDescription.textContent;
+    popup.classList.add('popup_opened');
+}
+
+function closePopup() {
     popup.classList.remove('popup_opened');
-    }
-    else {
-        popup.classList.add('popup_opened');
-    }
 }
-function formSubmitHandler (evt) {
-    evt.preventDefault(); 
-    document.querySelector('.profile__title').textContent=formName.value;
-    document.querySelector('.profile__description').textContent=formDescription.value
-    togglePopup();
+function formSubmitHandler(evt) {
+    evt.preventDefault();
+    profileTitle.textContent = formName.value;
+    profileDescription.textContent = formDescription.value
+    closePopup();
 }
 
 
-formElement.addEventListener('submit', formSubmitHandler); 
-popupOpenButton.addEventListener('click', togglePopup);
-popupCloseButton.addEventListener('click', togglePopup);
+formElement.addEventListener('submit', formSubmitHandler);
+popupOpenButton.addEventListener('click', openPopup);
+popupCloseButton.addEventListener('click', closePopup);
