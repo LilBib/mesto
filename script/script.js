93d48a1a543870cdce11a0130191948a1d8bad36
LilBib <vovchiksuper@yandex.ru>
2021-12-19 14:13:30 +0300

    Исправление ошибок после первого ревью

--- script/script.js
+++ script/script.js
@@ -1,17 +1,18 @@
-let editFormElement = document.querySelector('.form_task_edit');
-let addFormElement = document.querySelector('.form_task_add');
-let editPopupOpenButton = document.querySelector('.profile__edit-button');
-let popupCloseButton = document.querySelectorAll('.popup__close-icon');
-let addPopupOpenButton = document.querySelector('.profile__add-button');
-let formName = document.querySelector('.form__item_section_name');
-let formDescription = document.querySelector('.form__item_section_description');
-let formPlace = document.querySelector('.form__item_section_place');
-let formLink = document.querySelector('.form__item_section_link');
-let profileTitle = document.querySelector('.profile__title');
-let profileDescription = document.querySelector('.profile__description');
-let cardPopup= document.querySelector('.popup_assignment_card');
-let editPopup= document.querySelector('.popup_assignment_edit');
-let addPopup= document.querySelector('.popup_assignment_add');
+const editFormElement = document.querySelector('.form_task_edit');
+const addFormElement = document.querySelector('.form_task_add');
+const editPopupOpenButton = document.querySelector('.profile__edit-button');
+const popupCloseButtons = document.querySelectorAll('.popup__close-icon');
+const addPopupOpenButton = document.querySelector('.profile__add-button');
+const formName = document.querySelector('.form__item_section_name');
+const formDescription = document.querySelector('.form__item_section_description');
+const formPlace = document.querySelector('.form__item_section_place');
+const formLink = document.querySelector('.form__item_section_link');
+const profileTitle = document.querySelector('.profile__title');
+const profileDescription = document.querySelector('.profile__description');
+const cardPopup= document.querySelector('.popup_assignment_card');
+const editPopup= document.querySelector('.popup_assignment_edit');
+const addPopup= document.querySelector('.popup_assignment_add');
+const elements = document.querySelector('.elements');
 const initialCards = [
     {
       name: 'Архыз',
@@ -45,47 +46,58 @@ function renderElements () {
     }); 
 }
 
+function createCard (name, link) {
+  const cardTemplate = document.querySelector('#element').content;
+  const cardElement= cardTemplate.querySelector('.element').cloneNode(true);
+  cardElement.querySelector('.element__card').src=link;
+  cardElement.querySelector('.element__description').textContent=name;
+  cardElement.querySelector('.element__card').setAttribute("alt",`${name}`);
+
+  cardElement.querySelector('.element__like-button').addEventListener('click', function(evt){
+      evt.target.classList.toggle('element__like-button_active');
+  });
+  cardElement.querySelector('.element__delete-button').addEventListener('click', function(evt){
+      evt.target.closest('.element').remove();
+  });
+  cardElement.querySelector('.element__card').addEventListener('click', function(evt){
+      openPopup(cardPopup);
+      cardPopup.querySelector('.popup__image').setAttribute('src',link);
+      cardPopup.querySelector('.popup__caption').textContent=name;
+      cardPopup.querySelector('.popup__image').setAttribute("alt",`${name}`);
+  });
+  return cardElement;
+}
 
 function addElement (name, link) {
-    const cardTemplate = document.querySelector('#element').content;
-    const cardElement= cardTemplate.querySelector('.element').cloneNode(true);
-    const elements=document.querySelector('.elements');
-    cardElement.querySelector('.element__card').src=link;
-    cardElement.querySelector('.element__description').textContent=name;
-    elements.append(cardElement);
-
-    cardElement.querySelector('.element__like-button').addEventListener('click', function(evt){
-        evt.target.classList.toggle('element__like-button_active');
-    });
-    cardElement.querySelector('.element__delete-button').addEventListener('click', function(evt){
-        evt.target.closest('.element').remove();
-    });
-    cardElement.querySelector('.element__card').addEventListener('click', function(evt){
-        cardPopup.classList.add('popup_opened');
-        cardPopup.querySelector('.popup__image').setAttribute('src',link);
-        cardPopup.querySelector('.popup__caption').textContent=name;
-    });
+  const cardElement=createCard(name, link);
+  elements.prepend(cardElement);   
 }
+
+function openPopup(popup) {
+  popup.classList.add('popup_opened');
+}
+
 function openEditPopup() {
   formName.value = profileTitle.textContent;
   formDescription.value = profileDescription.textContent;
-  editPopup.classList.add('popup_opened');
+  openPopup(editPopup);
 }
-function openAddPopup() {
-  addPopup.classList.add('popup_opened');
-}
-
-
 
 function closePopup() {
-  document.querySelector('.popup_opened').classList.remove('popup_opened');
+  const openedPopup = document.querySelector('.popup_opened')
+  if (openedPopup) {
+    openedPopup.classList.remove('popup_opened');
+  }
+
 }
+
 function editFormSubmitHandler(evt) {
     evt.preventDefault();
     profileTitle.textContent = formName.value;
     profileDescription.textContent = formDescription.value;
     closePopup();
 }
+
 function addFormSubmitHandler(evt) {
     evt.preventDefault();
     addElement(formPlace.value,formLink.value);
@@ -98,10 +110,12 @@ renderElements();
 editFormElement.addEventListener('submit', editFormSubmitHandler);
 addFormElement.addEventListener('submit', addFormSubmitHandler);
 editPopupOpenButton.addEventListener('click', openEditPopup);
-addPopupOpenButton.addEventListener('click', openAddPopup);
-popupCloseButton.forEach(function (item){
+addPopupOpenButton.addEventListener('click', () => {
+    openPopup(addPopup);
+  });
+popupCloseButtons.forEach(function (item){
   item.addEventListener('click', closePopup);
 });
 
-
+// Геннадий, спасибо за очень быстрое и качественное ревью!!=)
     
